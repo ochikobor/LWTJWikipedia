caaca59755a5a4e05d14389e91a310cf69a2f27a
yusuke.nishimura <yusuke1.nishimura@lixil.com>
2022-07-15 07:59:44 +0000

    fix

--- resources/views/articles/article.blade.php
+++ resources/views/articles/article.blade.php
@@ -1,8 +1,10 @@
 <div class="card col-md-3 m-1">
     <div class="card-body">
-        <p class="card-subtitle mb-2">{{ $article->category }}</p>
+        <h4 class="badge badge-pill badge-primary">{{ $article->category }}</h4>
         <h5 class="card-title">{{ $article->title }}</h5>
-        <a class="card-text mr-4" href="{{ route('users.show',[$article->user]) }}">{{$article->user->name }}</a>
-        <a href="{{ route('articles.show', ['article'=>$article->id]) }}">詳細</a>
+        <a href="{{ route('users.show',[$article->user]) }}" class="badge badge-light">{{$article->user->name }}</a>
+        <div class="text-right">
+            <a href="{{ route('articles.show', ['article'=>$article->id]) }}">+ 詳細</a>
+        </div>
     </div>
 </div>
